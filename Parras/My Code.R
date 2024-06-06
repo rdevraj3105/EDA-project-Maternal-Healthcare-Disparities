59f6847df27dd74cf088598d718891955d161ba6
Samantha Parras <dmsparras@gmail.com>
2024-06-06 15:14:44 -0400

    test

---
+++ Parras/My Code.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+maternal <- read_csv("https://raw.githubusercontent.com/36-SURE/36-SURE.github.io/main/data/maternal.csv")
+
+# maternal tobacco use is associated with lower average birth weights 
+
+tobacco_data <- maternal %>% select() 
