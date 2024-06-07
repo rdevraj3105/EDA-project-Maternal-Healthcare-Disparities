f844fe17f604ff02de89ce8e1feb61b8510dde0e
Macey Kalmanek <macey.kalmanek@gmail.com>
2024-06-07 15:01:43 -0400

    commit

--- Kalmanek/Proposed hypothesis.R
+++ Kalmanek/Proposed hypothesis.R
@@ -1,6 +1,6 @@
 
 
-# Hypothesis 3: Is there a relationship between tobacco use and pre-pregnancy conditions like diabetes and hypertension?
+# Hypothesis 3: Is there an effect between tobacco use and the pre-pregnancy diabetes on a newborns birth rate?
 
 
 helloooo !! 
