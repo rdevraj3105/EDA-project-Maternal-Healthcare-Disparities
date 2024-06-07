ce0cb306d4dff52fcd56043b8f7c116d8fae0ca6
Samantha Parras <dmsparras@gmail.com>
2024-06-07 14:32:01 -0400

    hi

--- Kalmanek/Proposed hypothesis.R
+++ Kalmanek/Proposed hypothesis.R
@@ -3,5 +3,19 @@
 # Hypothesis 2: Are there differences in the average number of prenatal visits among different states?
 # Hypothesis 3: Is there a relationship between tobacco use and pre-pregnancy conditions like diabetes and hypertension?
 
+#from sam :)
+# Bar plot for pre-pregnancy diabetes
+maternal %>% 
+  ggplot(aes(x = TobaccoUse, fill = PrePregnancyDiabetes)) +
+  geom_bar() +
+  labs(x = "Tobacco Use",
+       y = "Proportion",
+       fill = "Pre-pregnancy Diabetes")
 
-helloooo !! 
+# Bar plot for pre-pregnancy hypertension
+maternal %>% 
+  ggplot(aes(x = TobaccoUse, fill = PrePregnancyHypertension)) +
+  geom_bar() +
+  labs(x = "Tobacco Use",
+       y = "Proportion",
+       fill = "Pre-pregnancy Hypertension")
