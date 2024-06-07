16906fb2a408df3e9eb714ca0ee75cec8f44947b
Macey Kalmanek <macey.kalmanek@gmail.com>
2024-06-07 15:00:17 -0400

    commit

--- Kalmanek/Proposed hypothesis.R
+++ Kalmanek/Proposed hypothesis.R
@@ -1,6 +1,5 @@
-# 3. Hypotheses/Questions
-# Hypothesis 1: Does maternal age correlate with birth weight?
-# Hypothesis 2: Are there differences in the average number of prenatal visits among different states?
+
+
 # Hypothesis 3: Is there a relationship between tobacco use and pre-pregnancy conditions like diabetes and hypertension?
 
 
